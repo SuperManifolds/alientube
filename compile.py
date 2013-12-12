84f69a549c3b3ae0a2e72bbe0ecced2123838c33
Alex Sørlie Glomsaas <alex@delexious.com>
2013-12-13 00:13:56 +0100

    Compilation now fully working

--- compile.py
+++ compile.py
@@ -1,44 +1,58 @@
 #!/usr/bin/python2.4
 import httplib, urllib, sys, os, shutil, platform, subprocess
-closureList = ['Firefox/data/script.js', 'Safari.safariextension/lib/script.js', 'Chrome/lib/script.js']
+closureList = ["Firefox/data/script.js", "Safari.safariextension/js/script.js", "Chrome/js/script.js"]
 
 # Get path to perform operation, if none default to working directory.
-path = sys.argv[1]
-if (path is None):
+if (len(sys.argv) > 1):
+    path = sys.argv[1];
+    os.chdir(path)
+else: 
     path = os.getcwd()
-
+    print "-- No path defined, using current working directory. --"
 # Delete previous compilation if any
-if (os.path.exists(os.path.join(path, 'gen')))
-    shutil.rmtree(os.path.join(path, 'gen'))
+if (os.path.exists(os.path.join(path, "gen"))):
+    print "-- Gen folder already exists: Erasing.. --"
+    shutil.rmtree(os.path.join(path, "gen"))
 
 # Ensure that required files are in the directories by performing the hardlink script
-if (platform.system() == 'Windows'):
-    subprocess.call([os.path.join(path, 'setup.bat')])
+print "-- Ensuring that all required libraries and resources are hardlinked. --"
+if (platform.system() == "Windows"):
+    p = subprocess.Popen("setup.bat")
+    p.wait()
 else:
-    subprocess.call('./' + [os.path.join(path, 'setup.sh')])
+    p = subprocess.Popen("./setup.sh")
+    p.wait()
 
 # Recursively copy the required directories to the result folder
-os.makedirs(os.path.join(path, 'gen'))
-shutil.copytree(os.path.join(path, 'Safari.safariextension'), os.path.join(path, 'gen', 'Safari.safariextension'))
-shutil.copytree(os.path.join(path, 'Safari.Chrome'), os.path.join(path, 'gen', 'Chrome'))
-shutil.copytree(os.path.join(path, 'Safari.Firefox'), os.path.join(path, 'gen', 'Firefox'))
+print "-- Generating gen folder. --"
+os.makedirs(os.path.join(path, "gen"))
+print "-- Copying Safari Files --"
+shutil.copytree(os.path.join(path, "Safari.safariextension"), os.path.join(path, "gen", "Safari.safariextension"))
+print "-- Copying Chrome files --"
+shutil.copytree(os.path.join(path, "Chrome"), os.path.join(path, "gen", "Chrome"))
+print "-- Copying Firefox files --"
+shutil.copytree(os.path.join(path, "Firefox"), os.path.join(path, "gen", "Firefox"))
 
 # Send the code to Google Closure Compiler
-code = open(os.path.join(path, 'lib/script.js'), 'r').read()
+print "-- Compressing script.js --"
+code = open(os.path.join(path, "lib/script.js"), "r").read()
 params = urllib.urlencode([
-    ('js_code', code),
-    ('compilation_level', 'SIMPLE OPTIMIZATIONS'),
-    ('output_format', 'text'),
-    ('output_info', 'compiled_code')
+    ("js_code", code),
+    ("compilation_level", "SIMPLE OPTIMIZATIONS"),
+    ("output_format", "text"),
+    ("output_info", "compiled_code")
 ])
 headers = { "Content-type": "application/x-www-form-urlencoded" }
-conn = httplib.HTTPConnection('closure-compiler.appspot.com')
-conn.request('POST', '/compile', params, headers)
+conn = httplib.HTTPConnection("closure-compiler.appspot.com")
+conn.request("POST", "/compile", params, headers)
 response = conn.getresponse()
 data = response.read()
 conn.close()
-
 # Overwrite the files.
 for file in closureList:
-    open(os.path.join(path, 'gen', file), 'w') as minified:
-        minified.write(data)
+    minified = open(os.path.join(path, "gen", file), "w")
+    minified.write(data)
+    minified.close()
+    print "-- " + os.path.join(path, "gen", file) + " has been saved --"
+
+print "Compiling complete"
