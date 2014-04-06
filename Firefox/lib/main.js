9436ca1142aec37210c24e024fceaf197534d38c
Alex Sørlie Glomsaas <alex@delexious.com>
2014-04-06 04:59:05 +0200

    Remove dependency on underscore.js

--- Firefox/lib/main.js
+++ Firefox/lib/main.js
@@ -30,7 +30,6 @@ pageMod.PageMod({
         data.url("jquery-1.9.1.min.js"),
         data.url("mustache.js"),
         data.url("snuownd.js"),
-        data.url("underscore-min.js"),
         data.url("raven.min.js"),
         data.url("script.js"), 
     ],
