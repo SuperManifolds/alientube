60c2e9a503b7a69f08d120ed6ed4d07471b99b31
Alex Sørlie Glomsaas <alex@delexious.com>
2014-07-27 16:51:50 +0200

    Complete compilation script

--- compile.sh
+++ compile.sh
@@ -7,13 +7,22 @@ cp res/style.css Safari.safariextension/res/style.css
 cp res/style.css Firefox/data/style.css
 
 cp res/templates.html Chrome/res/templates.html
+cp res/templates.html Safari.safariextension/res/templates.html
+cp res/templates.html Firefox/data/templates.html
+
+
 
 tsc --out lib/script.js TypeScript/index.ts --removeComments --declaration --sourcemap
-cp js/script.js Chrome/js/script.js
+
+cp lib/script.js Chrome/js/script.js
 cp lib/script.js.map Chrome/js/script.js.map
+
 cp lib/script.js Safari.safariextension/js/script.js
+cp lib/script.js Safari.safariextension/js/script.js.map
+
 cp lib/script.js Firefox/data/script.js
+cp lib/script.js Firefox/data/script.map
 
 cp -fr TypeScript Chrome/
 
-afplay /System/Library/Sounds/Glass.aiff
+tput bel
