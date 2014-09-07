14a2b441ddcfaece307ada2d6a350b8f5fa4ecce
xlexi <alex@delexious.com>
2014-09-07 09:01:02 +0200

    Move cancel to the correct place. Add comments

--- TypeScript/RedditAPI/Report.ts
+++ TypeScript/RedditAPI/Report.ts
@@ -31,12 +31,16 @@ module AlienTube {
             var report_other = this.reportContainer.querySelector("label[for='report_other']");
             report_other.appendChild(document.createTextNode(Main.localisationManager.get("reportOther")));
 
+            /* Set localisation text for the submit button */
             var submitButton = this.reportContainer.querySelector(".at_report_submit");
             submitButton.appendChild(document.createTextNode(Main.localisationManager.get("submit")));
 
+            /* Set localisation text for the cancel button */
             var cancelButton = this.reportContainer.querySelector(".at_report_cancel");
             cancelButton.appendChild(document.createTextNode(Main.localisationManager.get("cancel")));
 
+            /* Assign an event listener to all the buttons, checking if the one that is being selected is the "other" button.
+            If so, re-enable the "other reason" text field, if not, disable it. */
             var reportOtherButton = this.reportContainer.querySelector("#report_other");
             var reportOtherField = this.reportContainer.querySelector("#report_otherfield");
 
@@ -51,6 +55,8 @@ module AlienTube {
                 }, false);
             }
 
+            /* Submit button click event. Check if the currently selected radio button is the "other" button, if so retrieve it's text
+            field value. If not, use the value from whatever radio button is selected.  */
             submitButton.addEventListener("click", () => {
                 var activeRadioButton = this.getCurrentSelectedRadioButton();
                 var reportReason = "";
@@ -64,12 +70,12 @@ module AlienTube {
                     }
                 }
 
-                cancelButton.addEventListener("click", () => {
-                    this.reportContainer.parentNode.removeChild(this.reportContainer);
-                }, false);
-
+                /* Send the report to Reddit*/
                 new HttpRequest("https://api.reddit.com/api/report", RequestType.POST, () => {
                     if (isThread) {
+                        /* If the "thing" that was reported was a thread, we will iterate through the thread collection to find it, and
+                        delete it, effectively hiding it. We will then force a redraw of the tab container, selecting the first tab in
+                        the list.  */
                         var threadCollection = commentThread.commentSection.threadCollection;
                         for (var i = 0, len = threadCollection.length; i < len; i++) {
                             if (threadCollection[i].name === commentThread.threadInformation.name) {
@@ -82,6 +88,8 @@ module AlienTube {
                             }
                         }
                     } else {
+                        /* If the "thing" that was reported was a comment, we will locate it on the page and delete it from DOM,
+                        effectively hiding it. */
                         var comment = document.querySelector("article[data-reddit-id='" + thing.substring(3) + "']");
                         if (comment) {
                             comment.parentNode.removeChild(comment);
@@ -96,6 +104,12 @@ module AlienTube {
 
             }, false);
 
+            /* Cancel event listener, will merely just get rid of the report screen. */
+            cancelButton.addEventListener("click", () => {
+                this.reportContainer.parentNode.removeChild(this.reportContainer);
+            }, false);
+
+            /* Append the report screen to the appropriate location. */
             if (isThread) {
                 var parentContainer = document.querySelector("header .info");
                 parentContainer.appendChild(this.reportContainer);
@@ -103,9 +117,9 @@ module AlienTube {
                 var commentMain = document.querySelector("article[data-reddit-id='" + thing.substring(3) + "'] .at_commentmain");
                 commentMain.appendChild(this.reportContainer);
             }
-
         }
 
+        /* Method to iterate through the radio buttons and get the one with a selected (checked) status. */
         getCurrentSelectedRadioButton () {
             var radioButtonControllers = this.reportContainer.querySelectorAll("input[type=radio]");
             for (var i = 0, len = radioButtonControllers.length; i < len; i++) {
