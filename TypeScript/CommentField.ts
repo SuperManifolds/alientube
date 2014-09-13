b23491ee251af77874ec5b25bfec2ed855532ed1
xlexi <alex@delexious.com>
2014-09-13 11:07:16 +0200

    Add comments to the CommentField class

--- TypeScript/CommentField.ts
+++ TypeScript/CommentField.ts
@@ -19,36 +19,45 @@ module AlienTube {
         private previewElement : HTMLDivElement;
 
         constructor(parent : any, initialText? : string) {
+            /* Check if the paramter is a Coment Thread and assign the correct parent HTML element .*/
             if (parent instanceof CommentThread) {
                 this.parentClass = <CommentThread> parent;
                 this.commentThread = this.parentClass;
                 this.parentHTMLElement = this.parentClass.threadContainer.querySelector(".options");
 
+            /* Check if the parameter is a Comment and assign the correct parent HTML element.*/
             } else if (parent instanceof Comment) {
                 this.parentClass = <Comment> parent;
                 this.commentThread = this.parentClass.commentThread;
                 this.parentHTMLElement = this.parentClass.representedHTMLElement.querySelector(".options");
+
             } else {
                 new TypeError("parent needs to be type CommentThread or Type Comment");
             }
+
             var template = this.commentThread.commentSection.template.getElementById("commentfield").content.cloneNode(true);
             this.representedHTMLElement = template.querySelector(".at_commentfield");
 
+            /* Set the "You are now commenting as" text under the comment field. */
             var authorName = <HTMLSpanElement> this.representedHTMLElement.querySelector(".at_writingauthor");
             authorName.innerText = Main.localisationManager.get("commentfield_label_author", [Main.Preferences.get("username")]);
 
+            /* Set the button text and event listener for the submit button */
             var submitButton = <HTMLButtonElement> this.representedHTMLElement.querySelector(".at_submit");
             submitButton.innerText = Main.localisationManager.get("commentfield_button_submit");
             submitButton.addEventListener("click", this.onSubmitButtonClick.bind(this), false);
 
+            /* Set the button text and event listener for the cancel button */
             var cancelButton = <HTMLButtonElement> this.representedHTMLElement.querySelector(".at_cancel");
             cancelButton.innerText = Main.localisationManager.get("commentfield_button_cancel")
-            submitButton.addEventListener("click", this.onSubmitButtonClick.bind(this), false);
-
-            var previewHeader = <HTMLSpanElement> this.representedHTMLElement.querySelector(".at_preview_header");
-            previewHeader.innerText = Main.localisationManager.get("commentfield_label_preview");
             cancelButton.addEventListener("click", this.onCancelButtonClick.bind(this), false);
 
+            /* Set the text for the markdown preview header */
+            var previewHeader = <HTMLSpanElement> this.representedHTMLElement.querySelector(".at_preview_header");
+            previewHeader.innerText = Main.localisationManager.get("commentfield_label_preview");
+
+            /* Check if we were initialised with some text (most likely from the show source button) and add event listener for input
+            change */
             var inputField = <HTMLInputElement> this.representedHTMLElement.querySelector(".at_textarea");
             if (initialText) {
                 inputField.value = initialText;
@@ -66,11 +75,16 @@ module AlienTube {
 
         onSubmitButtonClick () {
             var inputField = <HTMLInputElement> this.representedHTMLElement.querySelector(".at_textarea");
-            var thing_id = (this.parentClass instanceof CommentThread) ? this.parentClass.threadInformation.name : this.parentClass.commentObject.name;
+            var thing_id = (this.parentClass instanceof CommentThread)
+                ? this.parentClass.threadInformation.name : this.parentClass.commentObject.name;
+
+            /* Send the comment to Reddit */
             new RedditCommentRequest(thing_id, inputField.value, (responseText) => {
                 var responseObject = JSON.parse(responseText);
                 var comment = new Comment(responseObject.json.data.things[0].data, this.commentThread);
                 this.parentClass.children.push(comment);
+
+                /* Find the correct insert location and append the new comment to DOM */
                 if (this.parentClass instanceof CommentThread) {
                     this.parentClass.threadContainer.appendChild(comment.representedHTMLElement);
                     new CommentField(this.parentClass);
@@ -78,7 +92,11 @@ module AlienTube {
                     var replyContainer = this.representedHTMLElement.querySelector(".at_replies");
                     replyContainer.appendChild(comment.representedHTMLElement);
                 }
+
+                /* Scroll the new comment in to view */
                 comment.representedHTMLElement.scrollIntoView(true);
+
+                /* The comment box is no longer needed, remove it and clear outselves out of memory */
                 this.representedHTMLElement.parentNode.removeChild(this.representedHTMLElement);
                 delete this;
             });
@@ -91,6 +109,8 @@ module AlienTube {
 
         onInputFieldChange(eventObject : Event) {
             var inputField = <HTMLInputElement> eventObject.target;
+
+            /* If there is any contents of the input box, display the markdown preview and populate it. */
             if (inputField.value.length > 0) {
                 this.previewElement.style.display = "block";
                 var previewContents = <HTMLDivElement> this.previewElement.querySelector(".at_preview_contents");
