90c15121616aa8142012ca8e4fa4c4cc15a0740b
xlexi <alex@delexious.com>
2015-05-04 17:13:26 +0200

    Clean up the comments of the BrowserPreferenceManager class.

--- TypeScript/BrowserPreferenceManager.ts
+++ TypeScript/BrowserPreferenceManager.ts
@@ -1,13 +1,14 @@
 /// <reference path="Utilities.ts" />
 /**
-    Namespace for All AlienTube operations.
-    @namespace AlienTube
+    * Namespace for All AlienTube operations.
+    * @namespace AlienTube
 */
 "use strict";
 module AlienTube {
     /**
-        Manages the Preferences across browsers.
-        @class BrowserPreferenceManager
+        * Manages the Preferences across browsers.
+        * @class BrowserPreferenceManager
+        * @param [callback] Callback for when the preferences has been loaded.
     */
     export class BrowserPreferenceManager {
         private preferences : Object;
@@ -25,11 +26,17 @@ module AlienTube {
             defaultDisplayAction: "alientube",
             channelDisplayActions: {}
         }
-
+    	
+        /**
+         * Load the preferences from the browser.
+         * @param [callback] Callback for when the preferences has been loaded.
+         * @constructor
+         */
         constructor (callback?) {
             this.preferences = {};
             switch (window.getCurrentBrowser()) {
                 case Browser.CHROME:
+                    /* Get the Chrome cloud sync preferences stored for AlienTube. */
                     chrome.storage.sync.get(null, (settings) => {
                         this.preferences = settings;
                         if (callback) {
@@ -39,6 +46,7 @@ module AlienTube {
                     break;
 
                 case Browser.FIREFOX:
+                    /* Get the Firefox preferences. */
                     this.preferences = self.options.preferences;
                     if (callback) {
                         callback(this);
@@ -46,6 +54,7 @@ module AlienTube {
                     break;
 
                 case Browser.SAFARI:
+                    /* Make a request to the global page to retreive the settings */
                     var listener = safari.self.addEventListener('message', function listenerFunction (event) {
                         if (event.name === "preferences") {
                             console.log("received");
@@ -64,37 +73,83 @@ module AlienTube {
             }
         }
 
-
+    	/**
+         * Retrieve a value from preferences, or the default value for that key.
+         * @private
+         * @warning Should not be used on its own, use getString, getNumber, etc, some browsers *cough* Safari *cough* will not give the value in the correct type.
+         * @param key The key of the preference item.
+         * @returns An object for the key as stored by the browser.
+         * @see getString getNumber getBoolean getArray getObject
+         */
         private get (key: string) : any {
-            return this.preferences[key] !== null && typeof (this.preferences[key]) !== 'undefined' ? this.preferences[key] : this.defaults[key];
+            if (this.preferences[key] !== null && typeof (this.preferences[key]) !== 'undefined') {
+                return this.preferences[key]
+            }
+            return this.defaults[key];
         }
-
-        getString (key : string) : string {
+    	
+        /**
+         * Retreive a string from preferences, or the default string value for that key.
+         * @param key the Key of the preference item.
+         * @returns A string for the key as stored by the browser.
+         * @see getNumber getBoolean getArray getObject
+         */
+        public getString (key : string) : string {
             return this.get(key);
         }
 
-        getNumber (key : string) : number {
+        /**
+         * Retreive a number from preferences, or the default numeric value for that key.
+         * @param key the Key of the preference item.
+         * @returns A number for the key as stored by the browser.
+         * @see getString getBoolean getArray getObject
+         */
+        public getNumber (key : string) : number {
             return parseInt(this.get(key), 10);
         }
 
-        getBoolean (key : string) : boolean {
+        /**
+         * Retreive a boolean value from preferences, or the default boolean value for that key.
+         * @param key the Key of the preference item.
+         * @returns A boolean for the key as stored by the browser.
+         * @see getString getNumber getArray getObject
+         */
+        public getBoolean (key : string) : boolean {
             return window.parseBoolean(this.get(key));
         }
 
-        getArray (key : string) : string[] {
+        /**
+         * Retreive an array from preferences, or the default array list for that key.
+         * @param key the Key of the preference item.
+         * @returns An array for the key as stored by the browser.
+         * @see getString getNumber getBoolean getObject
+         */
+        public getArray (key : string) : string[] {
             if (Array.isArray(this.get(key))) {
                 return this.get(key);
             }
             return JSON.parse(this.get(key));
         }
         
-        getObject (key : string) : Object {
+        /**
+         * Retreive an object from preferences, or the value for that key.
+         * @param key the Key of the preference item.
+         * @returns An object for the key as stored by the browser.
+         * @see getString getNumber getBoolean getArray
+         * @throws SyntaxError
+         */
+        public getObject (key : string) : Object {
             if (typeof this.get(key) === 'object') {
                 return this.get(key);
             }
             return JSON.parse(this.get(key));
         }
 
+        /**
+         * Insert or edit an item into preferences.
+         * @param key The key of the preference item you wish to add or edit.
+         * @param value The value you wish to insert.
+         */
         set (key: string, value: any): void {
             this.preferences[key] = value;
             switch (window.getCurrentBrowser()) {
@@ -118,8 +173,12 @@ module AlienTube {
             }
         }
 
-
-        get enforcedExludedSubreddits () {
+        /**
+         * Get a list of subreddits that will not be displayed by AlienTube, either because they are not meant to show up in searches (bot accunulation subreddits) or because they are deemed too unsettling.
+         * @private
+         * @returns An array list of subreddit names as strings.
+         */
+        private get enforcedExludedSubreddits () {
             return [
                 "mensrights",
                 "beatingcripples",
