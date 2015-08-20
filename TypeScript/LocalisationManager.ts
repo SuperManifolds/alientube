dab6bdce39f85cfc52f86589662ee8d4e59f8b67
Alex S. Glomsaas <alex@sorlie.co.uk>
2015-08-20 11:43:20 +0200

    Create function in localisation Manager for handling number plurality

--- TypeScript/LocalisationManager.ts
+++ TypeScript/LocalisationManager.ts
@@ -89,5 +89,19 @@ module AlienTube {
             }
             return "";
         }
+        
+        /**
+         * Retreive a localised string related to a number of items, localising plurality by language.
+         * @param key The key for the non-plural version of the string.
+         * @param value The number to localise by.
+         * @returns The requested language string.
+         */
+        public getWithLocalisedPluralisation(key : string, value : number) {
+            if (value > 1 || value === 0) {
+                return this.get(`${key}_plural`);
+            } else {
+                return this.get(key);
+            }
+        }
     }
 }
